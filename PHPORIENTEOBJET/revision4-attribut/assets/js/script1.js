f13033cc358cdc8b00408b1c9a018d7ef80eb159
Bruno <bvanmack@gmail.com>
2019-10-03 15:42:37 +0200

    attriut

--- PHPORIENTEOBJET/revision4-attribut/assets/js/script1.js
+++ PHPORIENTEOBJET/revision4-attribut/assets/js/script1.js
@@ -20,6 +20,13 @@ function clickHandler(evt) {
     console.log('clickHandler');
     evt.preventDefault(); // annule le comportement par defaut de mon element
     // changer le texte quand on click
+
+    console.log(this.getAttribute("href"));
+
+    const myHref = this.getAttribute("href");
+    const myArrayFromHref = myHref.split('/');
+    console.log(myArrayFromHref[(myArrayFromHref.length - 1)]);
+    refInteractivContainer.innerHTML =myArrayFromHref[(myArrayFromHref.length - 1)]
     
 }
 
